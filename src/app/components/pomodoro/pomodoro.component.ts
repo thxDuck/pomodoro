99e1af9898c2666edd4345329c74eb07799f8a4d
ThxDuck <dylan.pelle.dev@gmail.com>
2025-06-03 13:23:34 +0200

    Html layout Ok

---
+++ src/app/components/pomodoro/pomodoro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pomodoro',
+  imports: [],
+  templateUrl: './pomodoro.component.html',
+  styleUrl: './pomodoro.component.scss'
+})
+export class PomodoroComponent {
+
+}
