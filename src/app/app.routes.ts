bcaeb2b20131319aa2a76fa6b324a2aad326f023
ThxDuck <dylan.pelle.dev@gmail.com>
2025-06-03 13:23:34 +0200

    Html layout Ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { PomodoroComponent } from './components/pomodoro/pomodoro.component';
 
 export const routes: Routes = [
-	{ path: "", component: HomeComponent },
+	{ path: "", component: PomodoroComponent },
+	// { path: "config", component: HomeComponent },
+	// { path: "tasks", component: HomeComponent },
 ];
