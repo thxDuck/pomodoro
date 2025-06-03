e81c0d07c006e6f8e1e3cc066707ac7bb527d852
ThxDuck <dylan.pelle.dev@gmail.com>
2025-06-03 13:50:03 +0200

    Add some style to timer and task input

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { PomodoroComponent } from './components/pomodoro/pomodoro.component';
 
 export const routes: Routes = [
-	{ path: "", component: PomodoroComponent },
-	// { path: "config", component: HomeComponent },
-	// { path: "tasks", component: HomeComponent },
+	{ path: 'pomodoro', component: PomodoroComponent },
+	// { path: "config", component: ConfigComponent },
+	// { path: "tasks", component: TaskComponent },
 ];
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  selector: 'app-footer',
-  imports: [],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+	selector: 'app-footer',
+	imports: [RouterLink, RouterLinkActive],
+	templateUrl: './footer.component.html',
+	styleUrl: './footer.component.scss',
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
