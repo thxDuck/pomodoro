eb1a5582635b0740413a3f5a0810b23f69f30431
ThxDuck <dylan.pelle.dev@gmail.com>
2025-06-04 13:16:46 +0200

    Start adding style on button and timer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { PomodoroComponent } from './components/pomodoro/pomodoro.component';
 
 export const routes: Routes = [
-	{ path: 'pomodoro', component: PomodoroComponent },
+	{ path: '', component: PomodoroComponent },
 	// { path: "config", component: ConfigComponent },
 	// { path: "tasks", component: TaskComponent },
 ];
