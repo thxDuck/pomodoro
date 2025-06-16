d679824ad8fb73358acb1bd1073869e8373f4809
ThxDuck <dylan.pelle.dev@gmail.com>
2025-06-16 13:12:16 +0200

    pomodoro: Create model

---
+++ src/app/models/pomodoro-model.ts
@@ -0,0 +1,28 @@
+import { PomodoroSettings, SecondDurations, State } from './pomodoro-types';
+
+export class Pomodoro {
+	public focusTime: number;
+	public shortBreakTime: number;
+	public longBreakTime: number;
+	public focusSessions: number;
+	public autoStartBreak: boolean;
+	public autoStartFocus: boolean;
+
+	public state: State = 'paused';
+
+	constructor({
+		focusTime = SecondDurations.Minute * 25,
+		shortBreakTime = SecondDurations.Minute * 5,
+		longBreakTime = SecondDurations.Minute * 15,
+		focusSessions = 4,
+		autoStartBreak = false,
+		autoStartFocus = false,
+	}: Partial<PomodoroSettings>) {
+		this.focusTime = focusTime;
+		this.shortBreakTime = shortBreakTime;
+		this.longBreakTime = longBreakTime;
+		this.focusSessions = focusSessions;
+		this.autoStartBreak = autoStartBreak;
+		this.autoStartFocus = autoStartFocus;
+	}
+}
