1a44933908dfa465e0515fc47ce70edf7c934e80
Dylan <dylan.pelle@bodet-timesport.com>
2025-07-02 13:33:31 +0200

    Configuration : start design with a simple input

--- src/app/components/pages/configuration/configuration.component.ts
+++ src/app/components/pages/configuration/configuration.component.ts
@@ -1,9 +1,19 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
 	selector: "app-configuration",
-	imports: [],
+	imports: [ReactiveFormsModule],
 	templateUrl: "./configuration.component.html",
 	styleUrl: "./configuration.component.scss",
 })
-export class ConfigurationComponent {}
+export class ConfigurationComponent {
+	pomodoroForm = new FormGroup({
+		focusTime: new FormControl(25),
+		shortBreakTime: new FormControl(5),
+		longBreakTime: new FormControl(15),
+		focusSessions: new FormControl(4),
+		autoStartBreak: new FormControl(false),
+		autoStartFocus: new FormControl(false),
+	});
+}
