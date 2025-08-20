327df88443cf93cff5e6674c058b1c116a2645f9
ThxDuck <dylan.pelle.dev@gmail.com>
2025-08-20 13:51:28 +0200

    feat(configuration): Binging settigs with inputs, and update or cancel form

--- src/app/components/pages/configuration/configuration.component.ts
+++ src/app/components/pages/configuration/configuration.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 import { PomodoroSettings } from "../../../models/pomodoro-types";
 import { PomodoroConfigService } from "../../../services/pomodoro-config.service";
 
@@ -9,9 +9,38 @@ import { PomodoroConfigService } from "../../../services/pomodoro-config.service
 	styleUrl: "./configuration.component.scss",
 })
 export class ConfigurationComponent {
-public pomodoroSettings: PomodoroSettings;
+	public pomodoroSettings = signal<PomodoroSettings>({
+		focusTime: 25,
+		shortBreakTime: 5,
+		longBreakTime: 15,
+		focusSessions: 4,
+		autoStartBreak: false,
+		autoStartFocus: false,
+	});
+	initalConfig: PomodoroSettings;
 
 	constructor(private configurationService: PomodoroConfigService) {
-		this.pomodoroSettings = { ...this.configurationService.config() };
+		this.pomodoroSettings.set(this.configurationService.config());
+		this.initalConfig = { ...this.configurationService.config() };
+	}
+	public saveConfiguration() {
+		console.log("this.pomodoroSettings : ", this.pomodoroSettings());
+		this.configurationService.updateConfig(this.pomodoroSettings());
+	}
+	public resetConfiguration() {
+		console.log("resetConfiguration");
+		this.pomodoroSettings.set(this.initalConfig);
+	}
+
+	/**
+	 * Called by template `(input)` binding, so, each time the value is changed in the input, this method is called
+	 * @param key Key of PomodoroSettings
+	 * @param value New Value
+	 */
+	public updateSetting<K extends keyof PomodoroSettings>(
+		key: K,
+		value: PomodoroSettings[K],
+	) {
+		this.pomodoroSettings.update((s) => ({ ...s, [key]: value }));
 	}
 }
